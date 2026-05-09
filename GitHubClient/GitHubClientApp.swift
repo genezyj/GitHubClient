8aca2f212b6ab55f0c82f76855f130a9bb8494a7
genezyj <gene.zhang.23@gmail.com>
2026-05-09 17:15:26 +0800

    Initial Commit

---
+++ GitHubClient/GitHubClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubClientApp.swift
+//  GitHubClient
+//
+//  Created by Gene Zhang on 5/9/26.
+//
+
+import SwiftUI
+
+@main
+struct GitHubClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GitHubClientTests/GitHubClientTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubClientTests.swift
+//  GitHubClientTests
+//
+//  Created by Gene Zhang on 5/9/26.
+//
+
+import Testing
+@testable import GitHubClient
+
+struct GitHubClientTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
